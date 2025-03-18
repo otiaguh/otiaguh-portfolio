5326063a7d0a1bd908b2ebc4882a9388079c85d5
otiaguh <otiagosilva.profissional@gmail.com>
2025-03-18 21:59:07 +0000

    UPDATE - V.03.25.2
    - Changed Index to Oudate
    - Updated Index to Outdate Warn

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,21 +10,29 @@ const UpdatePage = () => {
   }, []);
 
   return (
-    <><Head>
-      <title>Portfólio Descontinuado</title>
-      <meta name="description" content="Portfólio pessoal." />
-      <link rel="icon" href="/icon.jpeg" />
-    </Head><div className="flex h-screen items-center justify-center bg-zinc-900 text-neutral-200">
-        <h1
-          className={`text-2xl md:text-4xl font-bold text-center transition-opacity duration-1000 ${fade ? "opacity-100" : "opacity-0"}`}
-        >
-          Este portfólio está a ser atualizado,{' '}
-          <Link href="/outdate">
-            <span className="text-lime-200 cursor-pointer hover:underline">clique aqui</span>
-          </Link>{' '}
-          para acessar o antigo.
-        </h1>
-      </div></>
+    <>
+      <Head>
+        <title>Portfólio Descontinuado</title>
+        <meta name="description" content="Portfólio pessoal." />
+        <link rel="icon" href="/icon.jpeg" />
+      </Head>
+      <div className="flex h-screen flex-col items-center justify-between bg-zinc-900 text-neutral-200 p-4">
+        <div className="flex flex-grow items-center justify-center">
+          <h1
+            className={`text-2xl md:text-4xl font-bold text-center transition-opacity duration-1000 ${fade ? "opacity-100" : "opacity-0"}`}
+          >
+            Este portfólio está a ser atualizado,{' '}
+            <Link href="/outdate">
+              <span className="text-lime-200 cursor-pointer hover:underline">clique aqui</span>
+            </Link>{' '}
+            para acessar o antigo.
+          </h1>
+        </div>
+        <div className="pb-4 text-center">
+          <h6 className="text-xs text-zinc-700">2025 © Tiago Vieira da Silva. All rights reserved.</h6>
+        </div>
+      </div>
+    </>
   );
 };
 
